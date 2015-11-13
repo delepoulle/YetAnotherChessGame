33065582670dd21acd93dffde591b8907de615a8
Samuel Delepoulle <delepoulle@univ-littoral.fr>
2015-11-13 16:51:36 +0100

    ajout des fichiers de description des package pour la javadoc

---
+++ src/echecs/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * le package echecs est un moteur de jeu d'échecs
+ */
+package echecs;
---
+++ src/gui/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * le package gui contient l'interface graphique du jeu d'échecs
+ */
+package gui;
